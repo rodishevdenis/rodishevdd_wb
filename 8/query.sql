d6c6dd48098727c9cef882c72a936370b0e68376
rodishev.denis <rodishev.denis@wb.ru>
2024-08-22 13:26:29 +0700

    fix query

--- 8/query.sql
+++ 8/query.sql
@@ -1,9 +1,10 @@
 select
-  toStartOfDay(dt),
+  toStartOfDay(parseDateTimeBestEffortOrNull(dt)),
   uniq(shk_id) qty_shk,
   uniq(tare_id) qty_tare,
   countIf(shk_id, tare_id ='0') qty_shk_without_tare,
-  countIf(shk_id, isstock = 1) qty_shk_stock,
+  countIf(shk_id, isstock = '1') qty_shk_stock,
   countIf(shk_id, state_id = 'WWE') qty_shk_wwe,
-  countIf(shk_id, place_cod = 0) qty_shk_without_pc 
-from SHKonPlace
+  countIf(shk_id, place_cod = '0') qty_shk_without_pc
+from default.SHKonPlace
+group by 1;
